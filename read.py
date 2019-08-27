ae84884b285bb58d4a48e9a3029c586a2d7b4b5a
Gundamwindcar <xubiying88@gmail.com>
2019-08-27 14:12:41 -0700

    add filter

--- read.py
+++ read.py
@@ -9,5 +9,11 @@ with open('reviews.txt','r') as f:
 		if count % 100000 == 0:
 			print(len(data))
 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+
 print('档案读取完了，总共有', len(data), '笔资料')
 print('留言平均长度为', sum_len / len(data))
+print('一共有', len(new), '笔留言长度小于100')
