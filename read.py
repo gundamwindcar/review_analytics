e8ad7931dfeef0e770c4c6850c0d847c84a369fe
Gundamwindcar <xubiying88@gmail.com>
2019-08-27 14:06:38 -0700

    add average lenth

--- read.py
+++ read.py
@@ -1,12 +1,13 @@
 data = []
 count = 0
+sum_len = 0
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
+		sum_len += len(line)
 		if count % 100000 == 0:
 			print(len(data))
 
-print(data[0])
-print('--------------------------')
-print(data[1])
+print('档案读取完了，总共有', len(data), '笔资料')
+print('留言平均长度为', sum_len / len(data))
