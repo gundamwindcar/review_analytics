231f031e6c679c02c73d76960b6cc7bcbf0597b0
Gundamwindcar <xubiying88@gmail.com>
2019-08-27 16:06:26 -0700

    add progressbar

--- read.py
+++ read.py
@@ -1,13 +1,16 @@
+import time
+import progressbar
+
 data = []
 count = 0
 sum_len = 0
+bar = progressbar.ProgressBar(max_value = 1000000)
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
 		sum_len += len(line)
-		if count % 100000 == 0:
-			print(len(data))
+		bar.update(count)
 print('档案读取完了，总共有', len(data), '笔资料')
 
 new = []
@@ -24,6 +27,7 @@ print('一共有', len(new), '笔留言长度小于100')
 print('一共有', len(good), '笔留言提到了good')
 
 #文字计数功能
+start_time = time.time()
 wc = {}
 for d in data:
 	words = d.split()
@@ -36,6 +40,9 @@ for d in data:
 for word in wc:
 	if wc[word] > 10000:
 		print(word, wc[word])
+
+end_time = time.time()
+print('花了', end_time - start_time, 'seconds')
 
 while True:
 	word = input('请问你想查什么字：')
